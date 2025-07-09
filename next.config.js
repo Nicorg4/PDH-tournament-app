8eba436a387275c1bf49834f5229b673f2ca15a4
Nicorg4 <nicolasgota@hotmail.com>
2025-07-08 21:01:25 -0300

    next config update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "dd1afbc334a5.ngrok-free.app",
+        hostname: "pdh-tournament-app-server-production.up.railway.app",
         pathname: "/images/**",
       },
     ],
